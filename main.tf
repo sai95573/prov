2baad3450b4f7e61738cc437ab5abf301d0740e5
saikrishna <sai95573@gmail.com>
2023-07-01 23:15:52 +0530

    added

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "prov-test" {
+  ami = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  key_name = "mumbai-key"
+ 
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("/home/saikrishna/.ssh/id_rsa")
+    host = self.public_ip
+  }
+  
+  provisioner "file" {
+    source = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/script.sh",
+      "/tmp/script.sh",
+    ]
+  }
+}
+
+
